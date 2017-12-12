39f7d251250df82a9476eb7ba862a44d5326b302
DESKTOP-NEVQU4R\SecMatrix <SecMatrix@hotmail.com>
2017-12-12 16:50:36 +0800

    主页壁纸

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   "./img/icon_wechat.png",
   "./img/avatar.jpg",
-  "./img/home-bg.jpg",
+  "./img/post-bg-unix-linux.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/syntax.css",
